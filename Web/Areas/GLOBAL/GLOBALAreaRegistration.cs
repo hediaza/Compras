775dadcbc70fb3fe79d65ff0511048798b306cdb
Hernan Diaz <hediaza@unal.edu.co>
2019-06-18 22:12:02 -0500

    Adicion area Global

---
+++ Web/Areas/GLOBAL/GLOBALAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Web.Areas.GLOBAL
+{
+    public class GLOBALAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "GLOBAL";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "GLOBAL_default",
+                "GLOBAL/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
